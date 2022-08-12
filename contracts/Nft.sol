9d520d6c677ad5bf6730ef2502d88f03fafe2972
Yaroslav Shakalov <yaroslavshakalov888@gmail.com>
2022-08-12 13:15:35 +0300

    modify nft Contract

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -1,39 +1,74 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Nft is ERC721 {
-    string public constant TOKEN_URI =
-        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
-    uint256 private s_tokenCounter;
+contract Nft is ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl {
+    event TokenMint(uint256 indexed tokenId);
+    event TokenBurn(uint256 indexed tokenId);
 
-    event DogMinted(uint256 indexed tokenId);
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC721("Dogie", "DOG") {
-        s_tokenCounter = 0;
+    constructor() ERC721("Yara", "YToken") {
+        _setupRole(MINTER_ROLE, msg.sender);
     }
 
-    function mintNft() public {
-        _safeMint(msg.sender, s_tokenCounter);
-        emit DogMinted(s_tokenCounter);
-        s_tokenCounter = s_tokenCounter + 1;
+    modifier isExists(uint256 tokenId) {
+        _exists(tokenId);
+        _;
+    }
+
+    function mint(address tokenOwner, string memory _uri)
+        public
+        payable
+        onlyRole(MINTER_ROLE)
+    {
+        uint256 tokenIndex = totalSupply();
+
+        _safeMint(tokenOwner, tokenIndex);
+        _setTokenURI(tokenIndex, _uri);
+
+        emit TokenMint(tokenIndex);
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        require(ownerOf(tokenId) == msg.sender);
+
+        super._burn(tokenId);
+
+        emit TokenBurn(tokenId);
     }
 
     function tokenURI(uint256 tokenId)
         public
         view
-        override
+        override(ERC721, ERC721URIStorage)
+        isExists(tokenId)
         returns (string memory)
     {
-        require(
-            _exists(tokenId),
-            "ERC721Metadata: URI query for nonexistent token"
-        );
-        return TOKEN_URI;
+        return super.tokenURI(tokenId);
     }
 
-    function getTokenCounter() public view returns (uint256) {
-        return s_tokenCounter;
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 }
